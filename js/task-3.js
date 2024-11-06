541e39aa69171d0577688920ce896d55a6a00329
NagorniyV <prometey2095@gmail.com>
2024-11-06 22:09:53 +0200

    add markup

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,29 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-  
-    changeUsername(newName) {
-        this.username = newName;
-    },
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
 
-    updatePlayTime(hours) {
-        this.playTime += hours;
-},
+    padEnd(str) {
+        this.#value += str;
+    }
+    
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
 
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
 }
-};
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
